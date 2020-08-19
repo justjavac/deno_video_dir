50645c61927fa37475c0d7d5c735807faa317dbe
justjavac <justjavac@gmail.com>
2020-08-19 23:28:49 +0800

    release v0.1.0

--- mod.ts
+++ mod.ts
@@ -1,15 +1,29 @@
-/**
- * Every exported symbol ideally should have a documentation line.
+/** Returns the path to the user's video directory.
  *
- * It is important that documentation is easily human readable,
- * but there is also a need to provide additional styling information to ensure
- * generated documentation is more rich text.
- * Therefore JSDoc should generally follow markdown markup to enrich the text.
- *
- * follow https://deno.land/std/style_guide.md
- *
- * @param foo - Description of non obvious parameter
+ * The returned value depends on the operating system and is either a string,
+ * containing a value from the following table, or `null`.
+ * 
+ * |Platform | Value               | Example                  |
+ * | ------- | ------------------- | ------------------------- |
+ * | Linux   | `XDG_VIDEOS_DIR`    | /home/justjavac/Videos    |
+ * | macOS   | `$HOME`/Movies      | /Users/justjavac/Movies   |
+ * | Windows | `{FOLDERID_Videos}` | C:\Users\justjavac\Videos |
  */
-export default function starter(foo: string): string {
-  return foo;
+export default function videoDir(): string | null {
+  switch (Deno.build.os) {
+    case "linux": {
+      return Deno.env.get("XDG_VIDEOS_DIR") ?? null;
+    }
+
+    case "darwin": {
+      const home = Deno.env.get("HOME");
+      if (home) return `${home}/Movies`;
+      break;
+    }
+
+    case "windows":
+      return Deno.env.get("FOLDERID_Videos") ?? null;
+  }
+
+  return null;
 }
